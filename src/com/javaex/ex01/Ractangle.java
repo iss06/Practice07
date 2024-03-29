88248245a4954f4598763e8460931d0b59b8faea
하이미디어학원 <@DESKTOP-LJU20GE>
2024-03-29 17:40:32 +0900

    0329

--- src/com/javaex/ex01/Ractangle.java
+++ src/com/javaex/ex01/Ractangle.java
@@ -1,29 +1,32 @@
 package com.javaex.ex01;
 
-public class Ractangle extends Shape{
-	
+public class Ractangle extends Shape {
+
 	private int width;
 	private int height;
-	
-	public Ractangle(String fillColor, int width, int height){
+
+	public Ractangle(String fillColor, int width, int height) {
 		super(fillColor);
-		this.width=width;
-		this.height=height;
-	}
-	
-	public void showInfo(){
-		System.out.println("[사각형]#면색:" +super.fillColor+ " "
-				+ " #가로:" + width 
-				+" #세로:" + height + "그렸습니다.");                                                                        
+		this.width = width;
+		this.height = height;
 	}
 
+	// 추상 클래스 상속시 내부의 추상 메서드는 반드시 구현해야 한다.
 	@Override
 	public void draw() {
-		
+		System.out.println("[사각형]#면색:" + super.fillColor + " " + "#가로:" + width + " #세로:" + height + "그렸습니다.");
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
+	
+	public int getHeight() {
+		return this.height;
+	}
+	
+	public void setWidth(int width) {
 		
 	}
-		
-
 }
-
 
